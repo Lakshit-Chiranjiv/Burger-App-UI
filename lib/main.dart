ebc96904290895045714bea105b76584781010cc
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-17 01:09:15 +0530

    button and spaces

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                     const SizedBox(
-                      height: 320,
+                      height: 300,
                     ),
                     Padding(
                       padding: const EdgeInsets.all(20),
@@ -108,7 +108,31 @@ class HomePage extends StatelessWidget {
                           ],
                         ),
                       ),
-                    )
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                      ),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 44,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.orange[400]),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'Sign Up',
+                            style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               )
