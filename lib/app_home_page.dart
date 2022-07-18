1b01a7fe37944d202a6588d6032f735963e0df97
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-07-18 13:53:12 +0530

    starter topbar container

---
+++ lib/app_home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppHomePage extends StatelessWidget {
+  const AppHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+
+        ),
+      ),
+    );
+  }
+}
